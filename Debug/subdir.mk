2a65957077116ff1aaa0c1d1261adc7c28239e01
William Lam <willmlam@agricola.(none)>
2011-04-14 17:56:34 -0700

    Changed to use boost maps

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/Users/willmlam/dev/workspaces/graphmod/aomdd/include" -I/opt/local/include -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -I"/home/willmlam/dev/graphmod/aomdd/include" -O2 -g -pg -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
